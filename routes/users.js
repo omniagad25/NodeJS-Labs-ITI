a8b62b7d0ab127c799bcfa311e69671ce702a48d
omniagad25 <81337480+omniagad25@users.noreply.github.com>
2024-02-15 23:56:38 +0200

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
 const { UsersController } = require('../controllers');
 const asyncWrapper = require('../lib/async-wrapper');
+const CustomError = require('../lib/custom-error');
+
+const secretKey = process.env.key || 'test';
 
 router.post('/', async (req, res, next) => {
   const [err, user] = await asyncWrapper(UsersController.create(req.body));
@@ -17,6 +21,21 @@ router.post('/login', async (req, res, next) => {
     return res.json(token);
   }
   return next();
+});
+
+router.use((req, res, next) => {
+  try {
+    const token = req.get('authorization');
+    const verified = jwt.verify(token, secretKey);
+    if (verified) {
+      // eslint-disable-next-line no-underscore-dangle
+      req.userId = verified.id;
+      return next();
+    }
+    throw new CustomError('UNAUTHORIZED', 401);
+  } catch (error) {
+    throw new CustomError('Invalid token', 401);
+  }
 });
 
 router.get('/', async (req, res, next) => {
